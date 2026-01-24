d78c5545383922a712973f3c4a51285bfa4a131e
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-01-24 19:09:55 +0530

    Refactor SQL script with new table structures and queries

--- question_subquery.sql
+++ question_subquery.sql
@@ -302,3 +302,21 @@ INSERT INTO country (city_id, city_name, country_name) VALUES
 (104, 'Toronto', 'Canada'),
 (105, 'Sydney', 'Australia');
 
+
+with my_cp as (
+  select * , avg(amount) over(order by p.customer_id) as "avg",
+	count(address_id) over(order by c.customer_id) as "count"
+  from payments as p 
+  inner join customers as c
+on p.customer_id = c.customer_id
+),
+my_ca as (
+  select * from customers as c
+inner join address as a
+  on a.address_id = c.address_id
+  inner join country as cc
+on cc.city_id = a.city_id
+)
+select cp.first_name, cp.last_name , ca.city_name, ca.country_name, cp.amount
+from my_ca as ca , my_cp as cp
+
