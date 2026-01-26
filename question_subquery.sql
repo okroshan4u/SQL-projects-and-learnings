cdf4f479c7257acc917cf3b663578850ab4dedfe
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-01-26 22:23:43 +0530

    Refactor SQL queries and table definitions

--- question_subquery.sql
+++ question_subquery.sql
@@ -388,13 +388,13 @@ INSERT INTO employee (emp_id, emp_name, manager_id) VALUES
 
 Employee
 emp_id	emp_name	manager_id
-1	Madhav	
-2	Sam		1
-3	Tom		2
-4	Arjun		6
-5	Shiva		4
-6	Keshav		1
-7	Damodar		5
+1		Madhav	
+2		Sam			1
+3		Tom			2
+4		Arjun		6
+5		Shiva		4
+6		Keshav		1
+7		Damodar		5
 
 
 ** Q ** Find the management chain (hierarchy) upwards for a given employee
@@ -459,5 +459,6 @@ emp_id	emp_name	manager_id	level
 3		Tom				2		2
 5		Shiva			4		3
 7		Damodar			5		4	
+
 
 
